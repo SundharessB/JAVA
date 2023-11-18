eced93d60d7486a1a52b2f6e6b10495671c29cd5
SundharessB <139948283+SundharessB@users.noreply.github.com>
2023-11-18 12:25:03 +0530

    Add files via upload

---
+++ ComplexPattern.java
@@ -0,0 +1,19 @@
+class a
+{
+public static void main(String args[])
+{
+System.out.println("A Main Method");
+}
+}
+class b
+{
+public static void  main(String args[])
+{
+System.out.println("B Main Method");
+}
+}
+class c
+{
+public static void main(String...args)
+{
+"a.java" 24L, 295B
